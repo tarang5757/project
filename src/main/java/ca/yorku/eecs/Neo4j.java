de0a7bf88540420231c51382c81b304ba7d7f500
Duarte Carreira <carreira711@gmail.com>
2024-07-22 00:02:10 -0400

    Update Neo4j
    Added without encryption config

--- src/main/java/ca/yorku/eecs/Neo4j.java
+++ src/main/java/ca/yorku/eecs/Neo4j.java
@@ -1,16 +1,19 @@
 package ca.yorku.eecs;
 import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Config;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 public class Neo4j implements AutoCloseable{
 	
+	public static Config config;
 	private Driver driver;
 	private String url;
 
     // Database neo4j constructor
     public Neo4j() {
         url = "bolt://localhost:7687";
-    	driver = GraphDatabase.driver(url, AuthTokens.basic("neo4j", "12345678"));
+        config = Config.builder().withoutEncryption().build();
+    	driver = GraphDatabase.driver(url, AuthTokens.basic("neo4j", "12345678"), config);
     }
 
     @Override
