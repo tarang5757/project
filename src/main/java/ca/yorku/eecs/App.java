08f0a72cbe5195e676a148b4a7ddb1674eaa6d95
tarang5757 <86339819+tarang5757@users.noreply.github.com>
2024-07-22 16:53:24 -0400

    Update App.java

--- src/main/java/ca/yorku/eecs/App.java
+++ src/main/java/ca/yorku/eecs/App.java
@@ -5,7 +5,7 @@ import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
 public class App {
-    static int PORT = 3000;
+    static int PORT = 8080;
 
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
